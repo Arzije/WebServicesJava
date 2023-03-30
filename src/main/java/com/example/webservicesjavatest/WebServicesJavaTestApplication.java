48bbfa3d8badb0caad624e34b0802d962f90a94b
Arzije <arzije.ziberovska@gmail.com>
2023-03-31 00:41:03 +0200

    New files

---
+++ src/main/java/com/example/webservicesjavatest/WebServicesJavaTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.webservicesjavatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesJavaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServicesJavaTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webservicesjavatest/WebServicesJavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webservicesjavatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesJavaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
